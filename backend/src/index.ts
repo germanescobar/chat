ca3a9e6ae2c5655ea58ba46f3acbf0c430fcf55a
Germán Escobar <german.escobarc@gmail.com>
2022-01-30 16:50:28 -0500

    Configure dotenv on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
+import * as dotenv from "dotenv"
 import express, { Express, Request, Response } from 'express'
 
+dotenv.config()
+
+const PORT = process.env.PORT
+
 const app: Express  = express()
+
+console.log(process.env.TEST)
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ status: "success" });
 } );
 
-app.listen(3001, () => console.log("Running ..."))
+app.listen(PORT, () => console.log(`Running on port ${PORT}...`))
